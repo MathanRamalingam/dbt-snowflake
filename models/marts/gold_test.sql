f1aad7c9d9e98696500afe840283998438d82f76
mathan@tradesolution.no <mathan@tradesolution.no>
2026-06-07 18:14:24 +0000

    workflow

--- models/marts/gold_test.sql
+++ models/marts/gold_test.sql
@@ -3,7 +3,7 @@
     (materialized = 'table')
 }}
 
-
+--this is price-
 select orderType , sum(coalesce(payerPrice , receiverPrice) )as price , count(*) as count
 from {{ref('int_price_clarification')}}
 group by orderType
